b4e9b4587ac6820d0d09948606e1ac6ea38378ad
desmondlee <desmondleekk95@gmail.com>
2026-08-21 22:18:06 +0800

    style: remove red and blue dot icons from team dropdown options in Game1DuelArenaModal

--- src/components/mixer/Game1DuelArenaModal.tsx
+++ src/components/mixer/Game1DuelArenaModal.tsx
@@ -108,7 +108,7 @@ export function Game1DuelArenaModal({
               >
                 {finalTeams.map(team => (
                   <option key={team.id} value={team.id} className="bg-white text-black">
-                    🔴 {getSafariTeamLabel(team.name)}
+                    {getSafariTeamLabel(team.name)}
                   </option>
                 ))}
               </select>
@@ -140,7 +140,7 @@ export function Game1DuelArenaModal({
               >
                 {finalTeams.map(team => (
                   <option key={team.id} value={team.id} className="bg-white text-black">
-                    🔵 {getSafariTeamLabel(team.name)}
+                    {getSafariTeamLabel(team.name)}
                   </option>
                 ))}
               </select>
